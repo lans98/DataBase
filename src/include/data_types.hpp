6b169ddf3fb4fd1ca3b74c0380435c2cbd06faf8
Kevin <lans9831@gmail.com>
2018-06-08 18:15:28 -0500

    refactored data type

--- src/include/data_types.hpp
+++ src/include/data_types.hpp
@@ -3,12 +3,36 @@
 
 #include <string>
 #include <variant>
+#include <utility>
+#include <stdexcept>
 
 namespace data_types {
-  using namespace std;
+    using namespace std;
 
-  using DataType = variant<
-    short, unsigned int, unsigned long, float, double, int, long, string>;
+    using DataType = variant<
+        short, 
+        unsigned int, 
+        unsigned long, 
+        float, 
+        double, 
+        int, 
+        long, 
+        string, 
+        bool,
+        char >;
+
+    enum Type : int {
+        SHORT,
+        UINT,
+        ULONG,
+        FLOAT,
+        DOUBLE, 
+        INT,
+        LONG,
+        STRING,
+        BOOL,
+        CHAR
+    };
 }
 
 #endif
