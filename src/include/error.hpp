ceb270e63a14423df3070eae0924a7f202170736
Kevin <lans9831@gmail.com>
2018-06-30 18:22:31 -0500

    error type, more extensible than exceptions

---
+++ src/include/error.hpp
@@ -0,0 +1,53 @@
+#ifndef SCPPDB_ERROR_HPP
+#define SCPPDB_ERROR_HPP
+
+#include <iostream>
+
+namespace error {
+
+    using namespace std;
+
+    enum ErrorKind : int {
+        NULL_STORAGE,
+        EMPTY_STORAGE, 
+        INCORRECT_PARAMS
+    };
+
+    class Error {
+    private:
+        ErrorKind kind;
+        string    message;
+
+    public:
+        Error() = delete;
+        Error(const Error&) = default;
+        Error(ErrorKind kind, string msg): kind(kind), message(msg) {}
+
+        friend ostream& operator<<(ostream& out, const Error& error) {
+            string kind_msg;
+
+            switch (error.kind) {
+                case NULL_STORAGE:
+                    kind_msg = "NULL_STORAGE";
+                    break;
+                case EMPTY_STORAGE:
+                    kind_msg = "EMPTY_STORAGE";
+                    break;
+                case INCORRECT_PARAMS:
+                    kind_msg = "INCORRECT_PARAMS";
+                    break;
+                default:
+                    kind_msg = "UNKNOWN_CODE";
+                    break;
+            }
+
+            out << "Error { kind: \"" << kind_msg << "\", "
+                << " code: " << error.kind  << ", "
+                << " message: \"" << error.message << "\" }";
+
+            return out;
+        } 
+    };
+}
+
+#endif
