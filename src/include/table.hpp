ce7d416e35f8a0992d39d79670918129ef625f27
Kevin <lans9831@gmail.com>
2018-06-30 18:29:25 -0500

    working on project method (relational algebra)

--- src/include/table.hpp
+++ src/include/table.hpp
@@ -141,8 +141,97 @@ namespace table {
             }
         }
 
-        string get_primary_key() const { return primary_key; }
-        void set_primary_key(string pk) { primary_key = move(pk); }
+        PrimaryKey get_primary_key() const { return primary_key; }
+        void set_primary_key(PrimaryKey pk) const { primary_key = move(pk); }
+
+        [[nodiscard]] // You shouldn't discard this return value
+        OperationResult project(const vector<Field>& sel_fields) {
+            // Simple cases where we don't return a result, just an error
+            if (!storage)
+                return { nullopt, Error(ErrorKind::NULL_STORAGE, "The storage doesn't exist for this table") };
+
+            if (storage->is_empty()) 
+                return { nullopt, Error(ErrorKind::EMPTY_STORAGE, "The storage exist but it's empty for this table") };
+
+            if (fields.size() < sel_fields.size())
+                return { nullopt, Error(ErrorKind::INCORRECT_PARAMS, "The vector of fields is bigger than the actual table fields size") };
+
+
+            // check if fields is equal to sel_fields
+            bool both_are_the_same = [&](){ 
+                if (fields.size() != sel_fields.size())
+                    return false;
+
+                auto itr1 = fields.begin();
+                auto itr2 = sel_fields.begin();
+                for (; itr1 != fields.end(); ++itr1, ++itr2)
+                    if (*itr1 != *itr2)                
+                        return false;
+
+                return true;
+            }();
+
+            if (both_are_the_same)
+                return { *this, nullopt };
+
+            // check if sel_fields are valid
+            bool sel_fields_are_valid = [&](){
+                auto itr1 = fields.begin();
+                auto itr2 = sel_fields.begin();
+                for (; itr1 != fields.end() || itr2 != sel_fields.end(); ++itr1, ++itr2)
+                    if (*itr1 != *itr2)
+                        return false;
+
+                return true;            
+            }();
+
+            if (!sel_fields_are_valid) 
+                return { nullopt, Error(ErrorKind::INCORRECT_PARAMS, "Some selected fields are invalid for this table") };
+
+            // check if the primary_key is present on the sel_fields if it isn't 
+            // or it's but partially (compound primary key) then use an auto counter as
+            // primary key for the result (this may change the order)
+            size_t counter_step = [&](){
+
+            }();
+                
+            if (counter_step)
+                result.fields.push_back(Field {
+                    .name = "_counter_",
+                    .type = ULONG,
+                    .index = 0,
+                    .visible = false
+                });
+
+            // If we are here, then we know that sel_fields size is less than fields size
+            // and that all selected fields are valid for this table now start iterating 
+            // over all the records and select just specified fields
+            Table result;
+
+            size_t counter = 0;
+            auto end = storage->end();
+            for (auto itr = storage->begin(); itr != end; ++itr) {
+                //Record& record = *itr;
+                vector<DataType> vals;
+                if (counter_step)
+                    vals.reserve(sel_fields.size() + 1);
+                else
+                    vals.reserve(sel_fields.size());
+
+                if (counter_step) {
+                    vals.push_back(counter);
+                    counter += counter_step;
+                }
+
+                for (auto& field : sel_fields) {
+                    auto it = find(fields.begin(), fields.end(), field);
+                    vals.push_back(itr->values[it->index]);
+                }
+            }
+
+            result.fields = sel_fields;
+            return result;
+        }
     };
 
 }
