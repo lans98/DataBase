83540cfd0f56136c13b7caecc8973dc829111ee4
Kevin <lans9831@gmail.com>
2018-07-03 23:34:11 -0500

    first impl of buffer

--- src/include/core/buffer.hpp
+++ src/include/core/buffer.hpp
@@ -1,1 +1,24 @@
 #pragma once
+
+#include <core/address.hpp>
+
+#include <set>
+
+namespace buffer {
+
+    using namespace std;
+    using namespace address;
+
+    template <class T>
+    class Buffer {
+    private:
+        set<Address<T>*> addresses;
+
+    public:
+        Buffer() = default;
+        Buffer(const Buffer&) = delete;
+
+        void push(Address<T>* address) {
+        }
+    };
+}
