07c95cc3795c45273e8603423a8299ed660ca83a
Kevin <lans9831@gmail.com>
2018-07-05 22:20:31 -0500

    added test2

--- test/address/read.cpp
+++ test/address/read.cpp
@@ -7,13 +7,22 @@ using namespace foo;
 using namespace address;
 
 void test1() {
-    printf("Test 1\n");
+    printf("Test 1:\n");
 
     auto foo = new Foo();
     Address<Foo> address(0UL, nullptr);
     cout << *address.read("foo_data") << '\n';
 }
 
+void test2() {
+    printf("Test 2:\n");
+
+    auto foo = new Foo();
+    Address<Foo> address(sizeof(*foo), nullptr);
+    cout << *address.read("foo_data") << '\n';
+}
+
 int main() {
     test1();
+    test2();
 }
