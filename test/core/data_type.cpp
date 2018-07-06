73c19bd82a9098587b3388332b5ee30b7e61c654
Kevin <lans9831@gmail.com>
2018-07-05 21:45:50 -0500

    test for new DataType

---
+++ test/core/data_type.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <iostream>
+
+#include <core/data_types.hpp>
+
+using namespace std;
+using namespace data_types;
+
+int main() {
+    DataType a(3);
+    DataType b(string("hola"));
+    DataType c(3.4);
+
+    auto a_value = a.get_int();
+    auto b_value = b.get_string();
+    auto c_value = c.get_double();
+
+    cerr << *a_value << '\n';
+    cerr << *b_value << '\n';
+    cerr << *c_value << '\n';
+
+    *a_value = 8;
+
+    cerr << *a_value << '\n';
+}
