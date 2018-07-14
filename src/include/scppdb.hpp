fb633135cab59f927295e90a1c04840bd5ace635
Kevin <lans9831@gmail.com>
2018-07-14 18:33:53 -0500

    DataBaseLoader in construction

--- src/include/scppdb.hpp
+++ src/include/scppdb.hpp
@@ -1,1 +1,30 @@
 #pragma once
+
+#include <config.hpp>
+
+namespace scppdb {
+
+    using namespace config;
+
+    class DataBaseLoader {
+    private:
+        ConfigInstance cfg;
+
+        static DataBaseLoader* instance;
+        DataBaseLoader(): cfg(Config::get_instance()) {}
+
+    public:
+        static DataBaseLoader& get_instance() {
+            if (!instance) 
+                instance = new DataBaseLoader;
+            return *instance;
+        }
+
+        void load() {
+                    
+        }
+    };
+
+    DataBaseLoader* DataBaseLoader::instance = nullptr;
+    using DataBaseLoaderInstance = DataBaseLoader&;
+}
