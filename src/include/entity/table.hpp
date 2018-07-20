f2edb626c2ee1b76c3ca6a16c624462855fd177c
jesuslazoquevedo <jesuslazoquevedo@gmail.com>
2018-07-20 02:30:20 -0500

    adding projection correctly

--- src/include/entity/table.hpp
+++ src/include/entity/table.hpp
@@ -28,11 +28,12 @@ namespace table {
     using namespace field;
     using namespace record;
     using namespace data_types;
+    using namespace comparison;
 
     /**
-     * Table is used to hold a vector of fields than 
-     * are the ID's for each of its colums, when we 
-     * are handling with records we can easily know 
+     * Table is used to hold a vector of fields than
+     * are the ID's for each of its colums, when we
+     * are handling with records we can easily know
      * the type of each column, because fields tell
      * us the type.
      */
@@ -52,30 +53,30 @@ namespace table {
         map<string, RecordStoragePtr> storage;
 
     public:
-        Table(): 
-            Entity(), 
-            name("_temp_"), 
-            pk_size(0UL), 
+        Table():
+            Entity(),
+            name("_temp_"),
+            pk_size(0UL),
             primary_key(),
             storage() {}
 
         Table(Table&&) = default;
         Table(const Table&) = default;
 
-        Table(string name, optional<EntityID> opt_parent = nullopt, optional<EntityID> opt_id = nullopt): 
-            Entity(EntityType::TABLE, opt_id, opt_parent), 
-            name(move(name)), 
-            pk_size(0UL), 
-            primary_key() {} 
+        Table(string name, optional<EntityID> opt_parent = nullopt, optional<EntityID> opt_id = nullopt):
+            Entity(EntityType::TABLE, opt_id, opt_parent),
+            name(move(name)),
+            pk_size(0UL),
+            primary_key() {}
             //storage(nullptr) {}
 
-        Table(string name, vector<Field> fields_list, optional<EntityID> opt_parent = nullopt, optional<EntityID> opt_id = nullopt): 
-            Entity(EntityType::TABLE, opt_id, opt_parent), 
-            name(move(name)), 
-            pk_size(0UL), 
+        Table(string name, vector<Field> fields_list, optional<EntityID> opt_parent = nullopt, optional<EntityID> opt_id = nullopt):
+            Entity(EntityType::TABLE, opt_id, opt_parent),
+            name(move(name)),
+            pk_size(0UL),
             primary_key()
-            //storage(nullptr) 
-        { 
+            //storage(nullptr)
+        {
             //copy(fields_list.begin(), fields_list.end(), fields.begin());
             for(int i=0; i<fields_list.size();i++){
                 fields.push_back(fields_list[i]);
@@ -83,7 +84,7 @@ namespace table {
         }
 
         PrimaryKey get_primary_key() const { return primary_key; }
-        void set_primary_key(PrimaryKey pk) { 
+        void set_primary_key(PrimaryKey pk) {
             for (auto& field: pk) {
                 auto it = find(fields.begin(), fields.end(), field);
                 if (it == fields.end())
@@ -91,7 +92,7 @@ namespace table {
             }
 
             pk_size = pk.size();
-            primary_key = move(pk); 
+            primary_key = move(pk);
         }
 
         string get_name() const { return name; }
@@ -104,7 +105,7 @@ namespace table {
                 //RecordStorage es la clase interfaz del b+
                 //es decir, acá creamos un b+
                 this->storage[field] = make_shared<RecordStorage>();
-                auto row_storage = this->storage[field];    
+                auto row_storage = this->storage[field];
 
                 //leo el archivo relacionado con esta tabla y voy insertando en índice, bajo hash
                 //se hace flush al terminar, ojo flush solo escribe en disco el árbol, pero este sigue estando en memoria
@@ -135,7 +136,7 @@ namespace table {
                     //insertamos
                     Address<size_t> reg(posicion_de_inicio_de_linea, nullptr);
                     row_storage->get_bplus().insert(valor_hash,reg);
-                    posicion_de_inicio_de_linea += linea.length() +1;   
+                    posicion_de_inicio_de_linea += linea.length() +1;
                 }
 
                 archivo_tabla.close();
@@ -143,16 +144,13 @@ namespace table {
             }
 
             cerr << "Ya está indexado el campo "<< field << ".\n";
-            return false; 
+            return false;
         }
 
-        /*
         Table projection(vector<Field> sel_fields) {
             // Simple cases where we don't return a result, just an error
-            if (!storage)
-                throw runtime_error("The storage doesn't exist for this table");
 
-            if (storage->is_empty()) 
+            if (storage.empty())
                 throw runtime_error("The storage exists but it's empty for this table");
 
             if (fields.size() < sel_fields.size())
@@ -163,34 +161,34 @@ namespace table {
             bool sel_fields_is_valid;
             bool present_primary_key;
 
-            { // inner scope to check the three above conditions
+             // inner scope to check the three above conditions
               // also used to updated selected fields types and visibility
-                both_are_the_same = (fields.size() == sel_fields.size());
+            both_are_the_same = (fields.size() == sel_fields.size());
 
-                auto itr = sel_fields.begin();
-                auto end = sel_fields.end();
+            auto itr = sel_fields.begin();
+            auto end = sel_fields.end();
 
-                size_t found_keys = 0;
-                
-                sel_fields_is_valid = true;
-                for (; itr != end; ++itr) {
-                    auto search = find(fields.begin(), fields.end(), *itr);
-                    if (search == fields.end()) {
-                        sel_fields_is_valid = false;
-                        break;
-                    }
+            size_t found_keys = 0;
+            sel_fields_is_valid = true;
+            for (; itr != end; ++itr) {
+              auto _search = find(fields.begin(), fields.end(), *itr);
+              if (_search == fields.end()) {
+                sel_fields_is_valid = false;
+                break;
+              }
 
-                    itr->set_type(search->get_type());
-                    itr->set_visible(true);
-                    auto it = find(primary_key.begin(), primary_key.end(), *itr);
-                    if (it != primary_key.end()) found_keys += 1;
-                }
-
-                both_are_the_same = (sel_fields_is_valid && both_are_the_same);
-                present_primary_key = (found_keys == pk_size) && pk_size;
+              itr->set_type(_search->get_type());
+              itr->set_visible(true);
+              auto it = find(primary_key.begin(), primary_key.end(), itr->get_name());
+              if (it != primary_key.end())
+                found_keys += 1;
             }
 
-            // handle results 
+            both_are_the_same = (sel_fields_is_valid && both_are_the_same);
+            present_primary_key = (found_keys == pk_size) && pk_size;
+
+
+            // handle results
             if (!sel_fields_is_valid)
                 throw runtime_error("Some selected fields are invalid for this table");
 
@@ -198,40 +196,28 @@ namespace table {
             if (both_are_the_same)
                 return *this;
 
-            // Above conditions passed so we can finally create an empty table 
+            // Above conditions passed so we can finally create an empty table
             // that will hold the result
             Table result;
 
             // if the primary key isn't complete in the selected fields, then we
             // need to use an autocounter has primary key
-            if (!present_primary_key)
-                result.fields.push_back(Field("_counter_", ULONG, false));
+            if (!present_primary_key){
+              result.fields.push_back(Field("_counter_", ULONG, false));
+              for(auto& itr : sel_fields)
+                result.fields.push_back(itr);
+            }
+            else
+              result.fields = sel_fields;
 
             // set the step for the autocounter if needed
             size_t step_counter = present_primary_key? 0UL : 1UL;
             size_t auto_counter = 0UL;
 
-            auto end = storage->end();
-            for (auto itr = storage->begin(); itr != end; ++itr) {
-                vector<DataType> vals;
-                if (step_counter)
-                    vals.reserve(sel_fields.size() + 1);
-                else
-                    vals.reserve(sel_fields.size());
-
-                if (step_counter) {
-                    vals.push_back(DataType(auto_counter));
-                    auto_counter += step_counter;
-                }
-
-                for (auto& field : sel_fields) {
-                    auto it = find(fields.begin(), fields.end(), field);
-                    auto pos = (it - fields.begin());
-                    vals.push_back(itr->values[pos]);
-                }
+            string name_field;
+            for(auto itr = sel_fields.begin(); itr != end; ++itr){
+              result.storage[itr->get_name()] = this->storage[itr->get_name()];
             }
-
-            result.fields = sel_fields;
             if (present_primary_key) {
                 result.set_primary_key(primary_key);
             } else {
@@ -239,10 +225,72 @@ namespace table {
                 pk.push_back("_counter_");
                 result.set_primary_key(pk);
             }
-
             return result;
         }
-        */
+
+        Table selection(Field sel_field, Field other_field, bool func(DataType,DataType,Type)){
+          if (!storage)
+            throw runtime_error("The storage doesn't exist for this table");
+
+          if (storage->is_empty())
+            throw runtime_error("The storage exists but it's empty for this table");
+
+          if (sel_field.type != other_field.type)
+            throw runtime_error("Can't operate a comparions between two different Field's types");
+
+          auto fields_end = fields.end();
+          auto fields_begin = fields.begin();
+          auto itr_sel_field = find(fields_begin,fields_end,sel_field);
+          auto itr_other_field = find(fields_begin,fields_end,other_field);
+
+          if (itr_sel_field == fields_end)
+            throw runtime_error("Select field is not found in current table");
+
+          if (itr_other_field == fields_end)
+            throw runtime_error("Select field for comparison is not found in current table");
+
+          Table result;
+
+          auto storage_end = storage->end();
+          size_t size = fields.size();
+
+          return result;
+        }
+
+        template<typename T>
+        Table selection(Field sel_field, T constant, bool func(DataType,DataType,Type) ){
+          if (!storage)
+              throw runtime_error("The storage doesn't exist for this table");
+
+          if (storage->is_empty())
+              throw runtime_error("The storage exists but it's empty for this table");
+
+          auto fields_end = fields.end();
+          auto fields_begin = fields.begin();
+          auto itr_sel_field = find(fields_begin,fields_end,sel_field);
+
+          if (itr_sel_field == fields_end)
+            throw runtime_error("Select field is not found in current table");
+
+          Table result;
+          //auto storage_end = storage->end();
+          size_t size = fields.size();
+
+          return result;
+        }
+
+        Table union(Table otable){
+
+        }
+        Table intersection(Table otable){}
+
+        bool insetion(Record tupla){}
+        bool delete(Record tupla){}
+        bool update(Record tupla){}
+
+        void print(){
+
+        }
     };
 
 }
