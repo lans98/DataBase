a8e0897c4c33a3795e11627aa0eb7bacad6e2232
jesuslazoquevedo <jesuslazoquevedo@gmail.com>
2018-07-20 00:35:42 -0500

    main test table

--- test/entity/table.cpp
+++ test/entity/table.cpp
@@ -6,5 +6,11 @@ using namespace std;
 using namespace table;
 
 int main() {
-
+  Field first_name("first_name", STRING, true);
+  Field last_name("last_name", STRING, true);
+  Field phone_num("phone_num", STRING, true);
+  Field address("address", STRING, true);
+  Field salary("salary", UINT, true);
+  vector<Field> campos = {first_name,last_name,phone_num,address,salary};
+  Table("Alumnos",campos);
 }
